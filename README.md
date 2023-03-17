fa86fab83115a896a8e8dcbd7814b8f1eb398ba7
tgujar <tanmaygujar999@gmail.com>
2023-03-16 20:27:36 -0700

    added comments, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # seat-gen
+
+A seat randomizer for test taking.
+
+## Features:
+
+- Config file to specify seat naming in shorthand.
+- Match students based on if they are left or right handed.
+- Regex matching for seats to assign spacing between them with priority.
+
+## Spacing priority
+
+Seats are sorted first according to the `spacing_priority` specified in the config. Seats matching a regex expression at an earlier index in the `spacing_priority` array appear earlier in the sorted array.
+
+Spacing is then assigned to seats, starting with seats which appear earlier in the sorted array.
+
+**Note: config fields are explained in the config file**
--- assign.py
+++ assign.py
@@ -24,6 +24,10 @@ class SeatLoader:
 
     def assign_seats(self, EnrolledStudents):
         max_empty = len(self.seats) - len(EnrolledStudents.students)
+        if max_empty < 0:
+            raise Exception("Not enough seats for students")
+
+        # Sort seats by spacing priority, higher priority seats are assigned spacing first
         self.seats.sort(key=lambda seat: next(
             (i for (i, h) in enumerate(self.spacing_priority)
              if re.fullmatch(h, seat["name"]) != None),
@@ -46,21 +50,27 @@ class SeatLoader:
 
             order = [right, ambi, left]
             if self.seats[i]["dex"] == "L":
-                order = [left, ambi, right]
+                order = [left, ambi, right]  # prioritize left handed students
 
             pick = next((l for l in order if len(l) > 0), None)
+
+            # if no students left, break
             if pick == None:
                 break
+
             self.seats[i].update({
                 k: v for k, v in pick.pop().items() if k not in EnrolledStudents.filter_fields
             })
-            self.seats[i]["allocated"] = True
-        self.seats = natsorted(self.seats, key=lambda seat: seat["name"])
+
+            self.seats[i]["allocated"] = True  # mark seat as allocated
 
     def output_seats(self):
+        self.seats = natsorted(self.seats, key=lambda seat: seat["name"])
+
         keys = self.seats[0].keys()
         if self.filter_unallocated:
             keys = [key for key in self.seats[0].keys() if key != "allocated"]
+
         with open(self.output_file, "w") as f:
             writer = csv.DictWriter(f, fieldnames=keys, extrasaction='ignore')
             writer.writeheader()
@@ -78,9 +88,10 @@ class StudentLoader:
             self.dexCol = student_data["dex"]
             self.filter_fields = student_data["filter_fields"]
             with open(student_data["file"]) as p:
-                self.students = list(csv.DictReader(
-                    p, delimiter=student_data["delimiter"]))
-        random.shuffle(self.students)
+                self.students = list(
+                    csv.DictReader(p, delimiter=student_data["delimiter"]))
+
+        random.shuffle(self.students)  # shuffle students to randomize seating
 
 
 if __name__ == "__main__":
